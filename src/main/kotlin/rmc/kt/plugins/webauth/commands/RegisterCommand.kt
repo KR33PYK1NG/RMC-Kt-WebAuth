d9e562b483c4ea304dce33f195dee59bc6fdeacc
KR33PY <kr33pyrmc@gmail.com>
2021-12-01 00:00:01 +0300

    [1.0.3] Добавлено событие регистрации для обновы DailyBonus

--- src/main/kotlin/rmc/kt/plugins/webauth/commands/RegisterCommand.kt
+++ src/main/kotlin/rmc/kt/plugins/webauth/commands/RegisterCommand.kt
@@ -1,11 +1,13 @@
 package rmc.kt.plugins.webauth.commands
 
+import org.bukkit.Bukkit
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import rmc.kt.plugins.core.base.CommandBase
 import rmc.kt.plugins.core.helpers.ChatHelper
 import rmc.kt.plugins.core.helpers.LogHelper
 import rmc.kt.plugins.webauth.WebAuthPlugin
+import rmc.kt.plugins.webauth.events.PlayerRegisteredEvent
 import rmc.kt.plugins.webauth.helpers.AuthHelper
 import rmc.kt.plugins.webauth.helpers.WebHelper
 
@@ -20,6 +22,7 @@ class RegisterCommand: CommandBase() {
         }, {
             AuthHelper.unregisterTaskIfActive(sender.name)
             AuthHelper.authorizeForMinute(sender.name, (sender as Player).address!!.hostString)
+            Bukkit.getPluginManager().callEvent(PlayerRegisteredEvent(sender.name, sender.address!!.hostString))
             sender.sendMessage(ChatHelper.format("&aВы зарегистрировались, приятной игры!"))
             LogHelper.debug("${sender.name} (ip: ${sender.address!!.hostString}) registered through Web API")
             sender.sendMessage(ChatHelper.format("&7У вас не указана почта. Пароль не получится восстановить при потере.\n" +
